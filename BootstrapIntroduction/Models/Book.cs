28910234e663f12f06e8d92abdd6bc2dc89ddaf4
Pritam Hinger <pritamgenius24@gmail.com>
2017-08-29 16:26:52 +0530

    Add Database Initializer and add it to Global.asax.cs.

--- BootstrapIntroduction/Models/Book.cs
+++ BootstrapIntroduction/Models/Book.cs
@@ -9,7 +9,7 @@ namespace BootstrapIntroduction.Models
     {
         public int Id { get; set; }
         public int AuthorId { get; set; }
-        public string title { get; set; }
+        public string Title { get; set; }
         public string Isbn { get; set; }
         public string Synopsis { get; set; }
         public string Description { get; set; }
